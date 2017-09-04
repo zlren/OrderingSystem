74d3fd50ac881c30fcf03068c76f95851dfb4190
zlren <zlren2012@163.com>
2017-09-04 09:23:22 +0800

    项目搭建完成;配置日志框架

---
+++ src/main/java/lab/zlren/sell/repository/ProductCategoryRepository.java
@@ -0,0 +1,8 @@
+package lab.zlren.sell.repository;
+
+import lab.zlren.sell.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+
+}
